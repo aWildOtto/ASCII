155a0c869edd9c9809dedb1f65b20fe4655b99c8
Otto Hu <ottohu101@gmail.com>
2020-11-07 16:36:01 -0800

    temp save

--- src/app/Pages/chat-page/chat-page.component.ts
+++ src/app/Pages/chat-page/chat-page.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { UserService } from 'src/app/Services/user/user.service';
 
 @Component({
   selector: 'app-chat-page',
@@ -7,7 +9,20 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ChatPageComponent implements OnInit {
 
-  constructor() { }
+  constructor(
+    private us: UserService,
+    private route: ActivatedRoute,
+
+  ) {
+
+    this.route.params.subscribe((params) => {
+      this.us.getUser(params.uid, (ref) => {
+        const userData = ref[0];
+        console.log(userData);
+      });
+    });
+
+  }
 
   ngOnInit(): void {
   }
